370b78c6ad852ed911feb1d130643746d0f836f2
luckystar360 <ngocnhat95@gmail.com>
2020-04-12 21:15:56 +0700

    first commit

---
+++ test/test/AttributeShapefile.cs
@@ -0,0 +1,64 @@
+﻿using DotSpatial.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace test
+{
+    public static class AttributeShapefile
+    {
+        public static void updateAttributetoSupport(ref Shapefile shapefile, XmlNodeList listSupportsNode, MappingAttribute mapping_attribute)
+        {
+            shapefile.Attributes.Table.Clear();
+            int index_row = 1;
+            foreach (XmlNode SupportsNode in listSupportsNode)
+            {
+                foreach (XmlNode SupportNode in SupportsNode)
+                {
+                    DataRow newRow = shapefile.Attributes.Table.NewRow();
+
+                    string zeroColumnName = "PROPRIETE";
+                    string idColumnName = "ID";
+                    if (!shapefile.Attributes.Table.Columns.Contains(zeroColumnName))
+                    {
+                        DataColumn zeroColumn = new DataColumn(zeroColumnName, typeof(string));
+                        zeroColumn.MaxLength = 50;
+                        shapefile.Attributes.Table.Columns.Add(zeroColumn);
+                    }
+                    if (!shapefile.Attributes.Table.Columns.Contains(idColumnName))
+                    {
+                        DataColumn idColumn = new DataColumn(idColumnName, typeof(string));
+                        idColumn.MaxLength = 50;
+                        shapefile.Attributes.Table.Columns.Add(idColumn);
+                    }
+                    newRow[zeroColumnName] = "ENEDIS";
+                    newRow[idColumnName] = index_row++;
+                    foreach (XmlNode attributeNode in SupportNode)
+                    {
+                        //Console.WriteLine(attributeNode.Name + ":" + attributeNode.InnerText);
+                        string columName = mapping_attribute.Mapping(attributeNode.Name).ToUpper();
+                        string nodeValue = attributeNode.InnerText;
+                        int index_comma = nodeValue.IndexOf(",");
+                        if (index_comma >= 0)
+                        {
+                            nodeValue = nodeValue.Replace(',', '.');
+                        }
+                        if (!shapefile.Attributes.Table.Columns.Contains(columName))
+                        {
+                            DataColumn newCol = new DataColumn(columName, typeof(string));
+                            newCol.MaxLength = 50;
+                            shapefile.Attributes.Table.Columns.Add(newCol);
+                        }
+                        newRow[columName] = nodeValue;
+                    }
+                    shapefile.Attributes.Table.Rows.Add(newRow);
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ test/test/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotSpatial.Projections;
+using DotSpatial.Data;
+using NUnit.Framework;
+using System.Xml;
+using System.Collections;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private string default_prj_out = @"qgis_file\DEFAULT_OUTPUT.prj";
+        private string mapping_file = @"qgis_file\MAPPING.xlsx";
+        XmlDocument pcm_doc;
+        ProjectionInfo info_output;
+        Shapefile shapefile;
+        MappingAttribute mapping_attribute;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mapping_attribute = new MappingAttribute();
+
+            info_output = ProjectionInfo.Open(default_prj_out);
+            if (!mapping_attribute.readFileExcel(mapping_file))
+            {
+                //mapping_data = mapping_attribute.mapping_data;
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// import shapefile
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog shpOpenFileDialog = new OpenFileDialog())
+            {
+                shpOpenFileDialog.Filter = "shp|*.shp";
+                shpOpenFileDialog.Title = "Import ShapeFile";
+                shpOpenFileDialog.Multiselect = false;
+                //openFileDialog1.InitialDirectory = lastLogDir ?? Properties.Settings.Default.LogDir;
+
+                if (shpOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //shapeFileOpen_name = shpOpenFileDialog.SafeFileName;
+                    shapefile = Shapefile.OpenFile(shpOpenFileDialog.FileName);
+                    shapefile.Reproject(info_output);
+
+                    return;
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// import pcm file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog pcmOpenFileDialog = new OpenFileDialog())
+            {
+                pcmOpenFileDialog.Filter = "PCM|*.pcm";
+                pcmOpenFileDialog.Title = "Import PCM File";
+                pcmOpenFileDialog.Multiselect = true;
+                //openFileDialog1.InitialDirectory = lastLogDir ?? Properties.Settings.Default.LogDir;
+
+                if (pcmOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pcm_doc = new XmlDocument();
+                    try
+                    {
+                        pcm_doc.Load(pcmOpenFileDialog.FileName);
+
+                        //selectForm_Commited(null);
+                    }
+                    catch
+                    {
+
+                    }
+                    return;
+                }
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// export shape file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlNodeList listSupportsNode = pcm_doc.SelectNodes("//Supports");
+                AttributeShapefile.updateAttributetoSupport(ref shapefile, listSupportsNode, mapping_attribute);
+            }
+            catch
+            {
+                MessageBox.Show("Error update attribute from pcm to shape file");
+            }
+            using (SaveFileDialog shpSaveFileDialog = new SaveFileDialog())
+                {
+                    shpSaveFileDialog.Filter = ".shp(ShapeFile)|*.shp";
+                    shpSaveFileDialog.Title = "Save a shapeFile";
+                    if (shpSaveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        if (shpSaveFileDialog.FileName != null)
+                        {
+                            shapefile.SaveAs(shpSaveFileDialog.FileName, true);
+                        }
+                    }
+
+                }
+            
+
+        }
+
+    }
+}
+
